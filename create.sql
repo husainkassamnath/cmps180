cc6c445592df848d234474d02c926ea1c6d431dc
Husain Abbasali Kassamnath <hkassamn@be-linux28.lt.ucsc.edu>
2016-09-28 17:28:26 -0700

    Lab1 cmps180

---
+++ create.sql
@@ -0,0 +1,51 @@
+
+
+
+
+CREATE TABLE Stores (
+
+store_id	INT PRIMARY KEY,
+address		TEXT,
+manager		TEXT,
+
+);
+
+
+
+CREATE TABLE Customers (
+
+customer_id	INT PRIMARY KEY,
+name		TEXT,
+address 	TEXT,
+email		TEXT,
+
+);
+
+
+
+CREATE TABLE Products (
+
+product_id	INT PRIMARY KEY,
+name 		TEXT,
+category	TEXT,
+manufacturer	TEXT,
+
+);
+
+
+
+
+CREATE TABLE Sales (
+
+store_id	INT PRIMARY KEY,
+customer_id	INT PRIMARY KEY,
+product_id	INT PRIMARY KEY,
+date		TEXT PRIMARY KEY,
+quantity	INT,
+price		INT,
+shipped		BOOLEAN, 
+
+PRIMARY KEY(store_id,  customer_id,  product_id,  date)
+
+);
+
